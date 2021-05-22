eaa16ddc0ebcf02c2577fac5bb5afd8b8a9e8822
Brian Ozawa Burns <BrianOzburns@gmail.com>
2021-05-22 11:40:32 -0700

    weird

---
+++ DFT/mapViewController.swift
@@ -0,0 +1,32 @@
+//
+//  mapViewController.swift
+//  DFT
+//
+//  Created by Brian Ozawa Burns on 5/22/21.
+//
+
+import UIKit
+teimport MapKit
+
+class mapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
