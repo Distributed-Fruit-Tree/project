40e9e574c9641537ea165338623cd5a252d714ff
Christopher LaBorde <ckelaborde@gmail.com>
2021-05-25 07:35:04 -0700

    Add Ability to edit Profile info in  Profile Editor View Controller

---
+++ DFT/EditProfileViewController.swift
@@ -0,0 +1,62 @@
+//
+//  EditProfileViewController.swift
+//  DFT
+//
+//  Created by Christopher LaBorde on 5/24/21.
+//
+
+import UIKit
+import Parse
+import AlamofireImage
+
+class EditProfileViewController: UIViewController {
+
+    
+    
+    @IBOutlet weak var editProfilePictureImageView: UIImageView!
+    @IBOutlet weak var editUsernameTextField: UITextField!
+    @IBOutlet weak var editBioTextField: UITextField!
+    @IBOutlet weak var editAddressTextField: UITextField!
+    @IBOutlet weak var editFruitOfferedTextField: UITextField!
+    
+    @IBAction func onSaveButton(_ sender: Any) {
+        let profile = PFObject(className: "Profile")
+        
+        profile["username"] = editUsernameTextField.text
+        profile["author"] = PFUser.current()!
+        
+        let imageData = editProfilePictureImageView.image!.pngData()
+        let file = PFFileObject(name: "image.png", data: imageData!)
+        
+        profile["image"] = file
+        
+        profile.saveInBackground { (success, error) in
+            if success {
+                self.dismiss(animated: true, completion: nil)
+                print("saved!")
+            } else {
+                print("error!")
+            }
+        }
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- DFT/ProfileViewController.swift
+++ DFT/ProfileViewController.swift
@@ -6,16 +6,53 @@
 //
 
 import UIKit
+import Parse
+import AlamofireImage
+import MessageInputBar
+
+
 
 class ProfileViewController: UIViewController {
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
+  
+    /*
+    func messageInputBar(_ inputBar: MessageInputBar, didPressSendButtonWith text: String) {
+            //create the comment
+            let profile = PFObject(className: "profile")
+            profile["bio"] = bio
+            profile["address"] = address
+            profile["name"] = name
+            profile["author"] = PFUser.current()!
 
+        
+        
+            selectedPost.add(profile, forKey: "profile")
+            selectedPost.saveInBackground{(success,error) in
+                if success{
+                    print("comment saved!")
+                }
+                else{
+                    print("error saving comment")
+                }
+            }
+            tableView.reloadData()
+
+            //clear and dismiss the input bar
+            commentBar.inputTextView.text = nil
+            showsCommentBar = false
+            becomeFirstResponder()
+            commentBar.inputTextView.resignFirstResponder()
+        }
+
+ */
+    
     /*
     // MARK: - Navigation
 
